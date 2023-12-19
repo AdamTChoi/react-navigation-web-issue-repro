566c856d4cae814972d58affaa7d366c7f0df51b
Adam Choi <101848460+AdamTChoi@users.noreply.github.com>
2023-12-19 13:51:13 -0600

    Add files via upload

---
+++ packages/ui/types/CustomToast.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+export declare const CustomToast: () => JSX.Element | null;
+//# sourceMappingURL=CustomToast.d.ts.map
---
+++ packages/ui/types/NativeToast.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+export declare const NativeToast: () => JSX.Element | null;
+//# sourceMappingURL=NativeToast.d.ts.map
